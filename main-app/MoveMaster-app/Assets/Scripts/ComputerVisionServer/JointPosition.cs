be92d4a198a08637c18e84f4a5bd8c001ef6ee0e
kwjay <kwjay1002@gmail.com>
2024-10-23 17:34:13 +0200

    [TASK-36] C# server for sending base64 and retrieving pose points
    [TASK-15] Refactor of computer vision server
    
    idk
    
    Add comments and fix shutting down mismatch
    
    Add namespace CVServer
    
    Fix adjusted joints array
    
    Fix naming of positions
    
    Fix name from plural to singular
    
    Add joints position class
    
    Add server health check
    
    Change comment to code
    
    Refactor
    
    Add shutdown signals to computer vision server
    
    Also changed the isRunning bool to httpListener.isListening()
    
    Add processed array return
    
    Add logic to computer vision server
    
    Add server endpoints
    
    Refactor
    
    Init

---
+++ main-app/MoveMaster-app/Assets/Scripts/ComputerVisionServer/JointPosition.cs
@@ -0,0 +1,18 @@
+
+namespace CVServer.Models
+{
+    public class JointPosition
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public bool flag { get; set; }
+
+        public JointPosition(float x, float y, bool flag)
+        {
+            this.x = x;
+            this.y = y;
+            this.flag = flag;
+        }
+    }
+
+}
