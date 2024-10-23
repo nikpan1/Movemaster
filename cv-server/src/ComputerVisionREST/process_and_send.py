83a9172ae7c289dc64e653dc005bee9c2744042d
kwjay <kwjay1002@gmail.com>
2024-10-23 17:34:13 +0200

    Init

---
+++ cv-server/src/ComputerVisionREST/process_and_send.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Frame(BaseModel):
+    image_base64: str
+
+class Settings(BaseModel):
+    threshold: float
+    model: str
+
+app = FastAPI()
+
+@app.post("/process")
+async def process_frame(frame: Frame):
+    image = frame.image_base64
+    return {"message": "Image received"}
+
+@app.post("/settings")
+async def settings():
+    return {"x": "y"}
+
+@app.post("/shutdown")
+async def shutdown():
+    return {"xyz"}
