8f46a106f148acebd3831991feddb74bb5b211ef
kwjay <kwjay1002@gmail.com>
2024-10-23 19:54:56 +0200

    Add logic to computer vision server

--- cv-server/src/ComputerVisionREST/process_and_send.py
+++ cv-server/src/ComputerVisionREST/process_and_send.py
@@ -1,5 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
+from numpy.ma.core import minimum
 from pydantic import BaseModel
+from ..Base64.Base64Conversions import *
+from ..PoseEstimation.PoseEstimation import PoseLandmarkExtractor
+
+pose_landmark_extractor = PoseLandmarkExtractor()
 
 class Frame(BaseModel):
     image_base64: str
@@ -12,13 +18,25 @@ app = FastAPI()
 
 @app.post("/process")
 async def process_frame(frame: Frame):
-    image = frame.image_base64
-    return {"message": "Image received"}
+    image = base64_to_image(frame.image_base64)
+    landmarks_array = pose_landmark_extractor.extract_landmarks(image)
+    return {
+        "positions": landmarks_array
+    }
 
 @app.post("/settings")
 async def settings(preset: Preset):
-    return {"x": "y"}
+    # minimum tracking confidence instead of model
+    pose_landmark_extractor = PoseLandmarkExtractor(preset.threshold, preset.model)
+    return {
+        "message": "Settings applied successfully",
+    }
 
 @app.post("/shutdown")
 async def shutdown():
-    return {"xyz"}
+    return {
+        "Goodbye"
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
