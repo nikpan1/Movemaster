3dc0cd4364470ebb084003625e70be9a1a992742
kwjay <kwjay1002@gmail.com>
2024-10-26 23:46:33 +0200

    Add server health check

--- cv-server/src/ComputerVisionREST/process_and_send.py
+++ cv-server/src/ComputerVisionREST/process_and_send.py
@@ -10,8 +10,13 @@ from ..PoseEstimation.PoseEstimation import PoseLandmarkExtractor
 
 UNITY_SERVER_URL = "http://localhost:7000"
 UNITY_SHUTDOWN_ENDPOINT = "/shutdown/"
+IS_UNITY_RUNNING = False
+
+
+app = FastAPI()
 
 pose_landmark_extractor = PoseLandmarkExtractor()
+
 class Frame(BaseModel):
     image_base64: str
 
@@ -19,7 +24,12 @@ class Preset(BaseModel):
     detection_confidence: float
     tracking_confidence: float
 
-app = FastAPI()
+@app.get("/health")
+async def health_check():
+    IS_UNITY_RUNNING = True
+    return {
+        "status" : "OK"
+    }
 
 @app.post("/process")
 async def process_frame(frame: Frame):
@@ -43,14 +53,15 @@ async def shutdown():
 
 @app.on_event("shutdown")
 async def shutdown_event():
-    async with httpx.AsyncClient() as client:
-        try:
-            unity_response = await client.post(
-                UNITY_SERVER_URL + UNITY_SHUTDOWN_ENDPOINT,
-                json = {"message": "Computer vision server is shutting down"}
-            )
-        except Exception as error:
-            print(f"Failed to send shutdown signal: {error}")
+    if IS_UNITY_RUNNING:
+        async with httpx.AsyncClient() as client:
+            try:
+                unity_response = await client.post(
+                    UNITY_SERVER_URL + UNITY_SHUTDOWN_ENDPOINT,
+                    json = {"message": "Computer vision server is shutting down"}
+                )
+            except Exception as error:
+                print(f"Failed to send shutdown signal: {error}")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
