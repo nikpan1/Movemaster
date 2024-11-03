7e8e99e26a7924e50b4c3e0b36601acb88cfa088
KWJ <102679975+kwjay@users.noreply.github.com>
2024-11-03 18:58:08 +0100

    Add server part [NOT TESTED]
    Can't test but need it on cloud

--- cv-server/src/MockerWebcamREST/mocker_webcam.py
+++ cv-server/src/MockerWebcamREST/mocker_webcam.py
@@ -1,10 +1,10 @@
-# import uvicorn
-# import httpx
+import uvicorn
+import httpx
 import os
-# import signal
-# from fastapi import FastAPI, BackgroundTasks
-# from pydantic import BaseModel
-# from src.Base64.Base64Conversions import *
+import signal
+from fastapi import FastAPI, BackgroundTasks
+from pydantic import BaseModel
+from src.Base64.Base64Conversions import *
 import cv2
 import tkinter as tk
 import PIL.Image, PIL.ImageTk
@@ -20,6 +20,7 @@ class MockerWB(tk.Tk):
         self.delay = 10
         self.playing = False
         self.webcam = False
+        self.current_frame = None
         self.update()
 
     def gui_setup(self):
@@ -78,9 +79,11 @@ class MockerWB(tk.Tk):
         if self.video_capture.video != None and self.playing:
             ret, frame = self.video_capture.get_frame()
             if ret:
+                self.current_frame = frame
                 self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
                 self.video_canvas.create_image(0,0, image = self.photo, anchor = tk.NW)
             else:
+                self.current_frame = None
                 self.playing = False
         self.after(self.delay, self.update)
 
@@ -170,5 +173,61 @@ class VideoCapture():
         if self.video != None and self.video.isOpened():
             self.video.release()
 
-mocker_webcam = MockerWB()
-mocker_webcam.mainloop()
+class Frame(BaseModel):
+    image_base64: str
+
+class MockerCaptureCameraServer:
+    def __init__(self) -> None:
+        self.UNITY_SERVER_URL = "http://localhost:7000"
+        self.UNITY_SHUTDOWN_ENDPOINT = "/shutdown/"
+        self.IS_UNITY_RUNNING = False
+        self.app = FastAPI()
+        self.mocker = MockerWB()
+
+
+    def start_server(self) -> None:
+        self.setup_calls()
+        self.mocker.mainloop()
+        uvicorn.run(self.app, host="127.0.0.1", port=8001)
+
+    def shutdown_server(self) -> None:
+        os.kill(os.getpid(), signal.SIGTERM)
+
+    def setup_calls(self) -> None:
+        @self.app.get("/health")
+        async def health_check():
+            self.IS_UNITY_RUNNING = True
+            return {
+                "status": "OK"
+            }
+
+        @self.app.post("/capture_and_send")
+        async def capture_and_send():
+            frame = self.mocker.current_frame
+            if frame == None:
+                raise Exception("Failed to capture frame")
+            image_base64 = image_to_base64(self.mocker.current_frame).decode('utf-8')
+            return image_base64
+
+        @self.app.post("/shutdown")
+        async def shutdown():
+            self.IS_UNITY_RUNNING = False
+            self.shutdown_server()
+            return {"message": "Capture Camera Server shutting down"}
+
+        @self.app.on_event("shutdown")
+        async def shutdown_event():
+            if not self.IS_UNITY_RUNNING:
+                async with httpx.AsyncClient() as client:
+                    try:
+                        await client.post(
+                            self.UNITY_SERVER_URL + self.UNITY_SHUTDOWN_ENDPOINT,
+                            json={"message": "Camera capture server is shutting down"}
+                        )
+                    except Exception as error:
+                        print(f"Failed to send shutdown signal: {error}")
+
+
+if __name__ == "__main__":
+    webcamImageServer = MockerCaptureCameraServer()
+    webcamImageServer.start_server()
