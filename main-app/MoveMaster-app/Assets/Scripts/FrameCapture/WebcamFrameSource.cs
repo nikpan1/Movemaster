fe367b266b3e28dc1439b315b04ccf8727e35fb0
nikpan1 <96376932+nikpan1@users.noreply.github.com>
2024-12-17 23:39:18 +0100

    [HOTFIX-140] Removed all external plugins

--- main-app/MoveMaster-app/Assets/Scripts/FrameCapture/WebcamFrameSource.cs
+++ main-app/MoveMaster-app/Assets/Scripts/FrameCapture/WebcamFrameSource.cs
@@ -4,12 +4,12 @@ using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
-using OpenCvSharp;
+// using OpenCvSharp;
 
 
 public class WebcamFrameSource : IFrameSource
 {
-    private VideoCapture _capture;
+    // private VideoCapture _capture;
     private Texture2D _texture;
     
     private bool _isInitialized = false;
@@ -22,12 +22,12 @@ public class WebcamFrameSource : IFrameSource
         Task.Run(() => { 
             Debug.Log("Initializing WebcamFrameSource. Please wait...");
             // @TODO: TASK-30 - `0` selects the default camera. Should be changed
-            _capture = new VideoCapture(0);
-
-            if (!_capture.IsOpened())
-            {
-                throw new Exception("WebcamFrameSource is not initialized.");
-            }
+            // _capture = new VideoCapture(0);
+            //
+            // if (!_capture.IsOpened())
+            // {
+            //     throw new Exception("WebcamFrameSource is not initialized.");
+            // }
 
             _isInitialized = true;
             Debug.Log("Initializing WebcamFrameSource. Ended :)");
@@ -38,11 +38,11 @@ public class WebcamFrameSource : IFrameSource
     {
         _isCapturing = false;
         
-        // Release VideoCapture resources.
-        _capture?.Release();
-        _capture?.Dispose();
-        _capture = null;
-        
+        // // Release VideoCapture resources.
+        // _capture?.Release();
+        // _capture?.Dispose();
+        // _capture = null;
+        //
         _texture = null;
     }
 
@@ -51,39 +51,39 @@ public class WebcamFrameSource : IFrameSource
         yield return new WaitUntil(() => _isInitialized);
         
         // Create a Texture2D with dimensions matching the captured frames.
-        _texture = new Texture2D(_capture.FrameWidth, _capture.FrameHeight, TextureFormat.RGB24, false); 
-
-        _isCapturing = true;
-        
-        Mat latestFrame = new Mat();
-        while (_isCapturing && _capture.IsOpened())
-        {
-            if (!_capture.Read(latestFrame) || latestFrame.Empty())
-            {
-                Debug.LogWarning("No frame captured.");
-                break;
-            }
-
-            MatToTexture(latestFrame, _texture);
-            triggers?.Invoke(_texture);
- 
-            yield return new WaitForSeconds(1 / Framerate);
-        }
-
-        if (_isCapturing)
-        {
-            throw new Exception("Capturing frames failed.");
-        }
-        
-        latestFrame.Release();
+        // _texture = new Texture2D(_capture.FrameWidth, _capture.FrameHeight, TextureFormat.RGB24, false); 
+        //
+        // _isCapturing = true;
+        //
+        // Mat latestFrame = new Mat();
+        // while (_isCapturing && _capture.IsOpened())
+        // {
+        //     if (!_capture.Read(latestFrame) || latestFrame.Empty())
+        //     {
+        //         Debug.LogWarning("No frame captured.");
+        //         break;
+        //     }
+        //
+        //     MatToTexture(latestFrame, _texture);
+        //     triggers?.Invoke(_texture);
+        //
+        //     yield return new WaitForSeconds(1 / Framerate);
+        // }
+        //
+        // if (_isCapturing)
+        // {
+        //     throw new Exception("Capturing frames failed.");
+        // }
+        //
+        // latestFrame.Release();
     }
 
-    private void MatToTexture(Mat mat, Texture2D texture)
-    {
-        // Convert the Mat to byte array.
-        byte[] byteArray = mat.ImEncode(".jpg");
-
-        // Load the byte array into the texture.
-        texture.LoadImage(byteArray);
-    }
+    // private void MatToTexture(Mat mat, Texture2D texture)
+    // {
+    //     // Convert the Mat to byte array.
+    //     byte[] byteArray = mat.ImEncode(".jpg");
+    //
+    //     // Load the byte array into the texture.
+    //     texture.LoadImage(byteArray);
+    // }
 }
