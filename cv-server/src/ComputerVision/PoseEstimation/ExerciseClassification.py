5dc36acf66bf2906b3586f4e202f56e9d3aabec1
nikpan1 <96376932+nikpan1@users.noreply.github.com>
2025-01-04 13:22:17 +0100

    [TASK-168] Improved model and optimized Video transmission

--- cv-server/src/ComputerVision/PoseEstimation/ExerciseClassification.py
+++ cv-server/src/ComputerVision/PoseEstimation/ExerciseClassification.py
@@ -7,109 +7,146 @@ from ComputerVision.PoseEstimation.model.multimodal_ae import MultimodalAutoenco
 from ComputerVision.PoseEstimation.model.multimodal_ar import MultimodalFcClassifier
 
 
+class Unit(object):
+    """Transforms the input sample and appends calculated values to expand dimensions."""
+
+    def __call__(self, sample):
+        additional_values = np.ones((sample.shape[0], sample.shape[1], 31 - sample.shape[2]))
+
+        for i in range(sample.shape[1]):  # Iterate over the second dimension
+            additional_values = Unit.calc_vec(additional_values, sample, 3, 4, 0, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 2, 3, 1, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 1, 2, 2, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 5, 1, 3, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 6, 5, 4, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 7, 6, 5, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 1, 8, 6, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 1, 11, 7, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 8, 9, 8, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 10, 8, 9, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 11, 12, 10, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 12, 13, 11, i)
+            additional_values = Unit.calc_vec(additional_values, sample, 0, 1, 12, i)
+
+        return np.concatenate((sample, additional_values), axis=2)
+
+    @staticmethod
+    def calc_vec(additional_values, sample, f, s, r, i):
+        additional_values[0, i, r] = abs(sample[0, i, f] - sample[0, i, s])
+        additional_values[1, i, r] = abs(sample[1, i, f] - sample[1, i, s])
+        return additional_values
+
 class Args:
     def __init__(self):
         self.num_classes = 11
-        self.window_length = 5
+        self.window_length = 3
         self.window_stride = 0.2
         self.skeleton_sampling_rate = 30
-        self.ae_layers = 3
-        self.ae_hidden_units = 1000
-        self.embedding_units = 1000
-        self.ae_dropout = 0.0
-        self.layers = 3
-        self.hidden_units = 100
-        self.dropout = 0.0
+        self.epochs = 150
+        self.ae_layers = 5
+        self.ae_hidden_units = 2048
+        self.embedding_units = 2048
+        self.ae_dropout = 0.1
+        self.layers = 5
+        self.hidden_units = 256
+        self.dropout = 0.1
+
+        self.lr = 1e-3
+        self.batch_size = 128
+        self.eval_every = 1
+        self.early_stop = 15
+        self.checkpoint = 10
 
 
 class ExerciseRecognition:
-    def __init__(self, model_path, device, repetitiveness, args):
-        self.model_path = model_path
-        self.device = device
-        self.repetitiveness = repetitiveness
-        self.repetitiveness_counter = 0
-        self.args = args
-
+    def __init__(self, model_path):
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.args = Args()
         self.pose_extractor = PoseLandmarkExtractor()
-
         self.labels_list = ['squats', 'lunges', 'bicep_curls', 'situps', 'pushups', 'tricep_extensions',
                             'dumbbell_rows', 'jumping_jacks', 'dumbbell_shoulder_press', 'lateral_shoulder_raises',
                             'non_activity']
-        self.inference_history = deque(maxlen=10)
         self.non_activity_counter = 0
+        self.model = self.load_model(model_path)
 
         self.predicted_class = 'non_activity'
         self.confidence = 0
 
-        self.model = self.load_model()
+    def load_model(self, model_path):
+        # Precompute skeleton window length
+        skeleton_window_length = int(self.args.window_length * self.args.skeleton_sampling_rate)
 
-    def load_model(self):
-        skel_model = ConvAutoencoder(input_size=(self.args.window_length, 16), input_ch=2, dim=2, layers=3,
-                                     grouped=[2, 2, 1], kernel_size=11, kernel_stride=(2, 1),
-                                     return_embeddings=True).to(self.device)
+        # Initialize skeleton model
+        skel_model = ConvAutoencoder(
+            input_size=(skeleton_window_length, 16),
+            input_ch=2,
+            dim=2,
+            layers=3,
+            grouped=[2, 2, 2],
+            kernel_size=5,
+            kernel_stride=(1, 1),
+            return_embeddings=True
+        ).to(self.device, non_blocking=True)
 
-        multimodal_ae_model = MultimodalAutoencoder(f_in=1620, skel=skel_model, layers=self.args.ae_layers,
-                                                    hidden_units=self.args.ae_hidden_units,
-                                                    f_embedding=self.args.embedding_units, dropout=self.args.ae_dropout,
-                                                    return_embeddings=True).to(self.device)
+        # Initialize multimodal autoencoder
+        multimodal_ae_model = MultimodalAutoencoder(
+            f_in=16200,
+            layers=5,
+            dropout=0.3,
+            hidden_units=128,
+            f_embedding=256,
+            skel=skel_model,
+            return_embeddings=False
+        ).to(self.device, non_blocking=True)
 
-        model = MultimodalFcClassifier(f_in=self.args.embedding_units, num_classes=self.args.num_classes,
-                                       multimodal_ae_model=multimodal_ae_model, layers=self.args.layers,
-                                       hidden_units=self.args.hidden_units, dropout=self.args.dropout).to(self.device)
+        # Initialize the classifier
+        model = MultimodalFcClassifier(
+            f_in=2880,
+            num_classes=self.args.num_classes,
+            multimodal_ae_model=multimodal_ae_model,
+            layers=5,
+            hidden_units=128,
+            dropout=0.3
+        ).to(self.device, non_blocking=True)
 
-        checkpoint = torch.load(self.model_path, map_location=self.device)
+        # Load and map checkpoint
+        checkpoint = torch.load(model_path, map_location=self.device)
         model.load_state_dict(checkpoint['model_state_dict'], strict=False)
+
+        # Set to evaluation mode
         model.eval()
 
         return model
 
     def process_frame(self, landmarks_history):
         if self.pose_extractor.is_full():
-            confidence, predicted_label = self.inference(landmarks_history)
+            self.confidence, self.predicted_class = self.inference(landmarks_history)
 
-            if predicted_label != 'non_activity' or confidence >= 0.66:
-                self.inference_history.append((predicted_label, confidence))
-                self.non_activity_counter = 0
-                return self.get_weighted_most_common_element()
-
-            else:
-                self.non_activity_counter += 1
-
-        if self.non_activity_counter >= 50 or not self.inference_history:
-            self.inference_history = self.inference_history[
-                                     -int(len(self.inference_history) / 2):] if self.inference_history else []
-            self.non_activity_counter = 0
-            return 'non_activity', 0
-
-        return self.get_weighted_most_common_element()
+        return self.predicted_class, self.confidence
 
     def inference(self, landmarks_history):
         input_data = PoseLandmarkExtractor.convert_history_to_tensor(landmarks_history, self.device)
+
+        # Ensure input_data has the correct shape for Unit processing
+        input_data_np = input_data.cpu().numpy()
+        unit_transform = Unit()
+        processed_data_np = unit_transform(input_data_np[0])
+
+        processed_data_tensor = torch.from_numpy(processed_data_np).unsqueeze(0).to(self.device, dtype=input_data.dtype)
+        input_data = processed_data_tensor
+
+        # Pass through the model
         output = self.model(input_data)
         _, predicted_class_idx = torch.max(output, dim=1)
         predicted_label = self.labels_list[predicted_class_idx.item()]
-        confidence = torch.softmax(output, dim=1)[0][predicted_class_idx].item()
-        return confidence, predicted_label
-
-    def get_weighted_most_common_element(self):
-        weighted_counter = Counter()
-        for label, confidence in self.inference_history:
-            weighted_counter[label] += confidence
-
-        most_common_element, total_confidence = weighted_counter.most_common(1)[0]
-        return most_common_element, total_confidence / 100
+        conf = torch.softmax(output, dim=1)[0][predicted_class_idx].item()
+        return conf, predicted_label
 
     def recognize(self, frame):
         self.pose_extractor.extract_landmarks(frame)
-
-        self.repetitiveness_counter += 1
-        if self.repetitiveness_counter >= self.repetitiveness:
-            self.repetitiveness_counter = 0
-
-            landmarks_history = self.pose_extractor.get_landmarks_history()
-            self.predicted_class, self.confidence = self.process_frame(landmarks_history)
-
-        return self.predicted_class, self.confidence
+        landmarks_history = self.pose_extractor.get_landmarks_history()
+        predicted_label, conf = self.process_frame(landmarks_history)
+        return predicted_label, conf
 
 
 if __name__ == '__main__':
