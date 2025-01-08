244d784d7cc9a978a3ce5f209f2b29754d855ee3
Olga Maruszyńska <olga.maruszynska@gmail.com>
2025-01-08 18:44:34 +0100

    First steps with gameplay

--- main-app/MoveMaster-app/Assets/Scripts/Widgets/Sidebar/Exercise.cs
+++ main-app/MoveMaster-app/Assets/Scripts/Widgets/Sidebar/Exercise.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Exercise", menuName = "Exercises/Add new exercise", order = 1)]
 public class Exercise : ScriptableObject
 {
-    [SerializeField] private int durationTime;
-    [SerializeField] private Sprite exerciseImage;
-
-    public int DurationTime => durationTime;
-    public Sprite ExerciseImage => exerciseImage;
+    [SerializeField] private string exerciseName;
+    [SerializeField] private Sprite exerciseSprite;
+    [SerializeField] private Animation exerciseAnimation;
+    
+    public string ExerciseName => exerciseName;
+    public Sprite ExerciseSprite => exerciseSprite;
+    public Animation ExerciseAnimation => exerciseAnimation;
 }
